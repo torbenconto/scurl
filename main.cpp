3204ca7608de82e524d6f2a63b41aa846264aa7d
torbenconto <torbenmconto@gmail.com>
2024-07-04 20:04:41 -0400

    only use ssl

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include <sys/socket.h>
 #include "strings.hpp"
 #include <netdb.h>
 #include <unistd.h>
 
 int main(int argc, char * argv[]) {
+    SSL_library_init();
+    SSL_load_error_strings();
+    SSL_CTX *ctx = SSL_CTX_new(SSLv23_client_method());
+
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <url> [port]\n";
         return 1;
@@ -34,7 +40,7 @@ int main(int argc, char * argv[]) {
         std::cout << "Path: " << path << std::endl;
     }
 
-    int port = 80; // Default port
+    int port = 443; // Default HTTPS port
 
     if (argc >= 3) {
         const char* portArg = argv[2];
@@ -56,8 +62,6 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-
-    // Returns status code
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         std::cerr << "Error creating sock\n";
@@ -65,7 +69,6 @@ int main(int argc, char * argv[]) {
     }
 
     sockaddr_in serverAddress{};
-    // IPv4
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(port);
     serverAddress.sin_addr = reinterpret_cast<sockaddr_in*>(res->ai_addr)->sin_addr;
@@ -75,28 +78,41 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    // Build an HTTP request
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sock);
+    SSL_set_tlsext_host_name(ssl, hostName.c_str());
+    SSL_set1_host(ssl, hostName.c_str());
+
+    if (SSL_connect(ssl) <= 0) {
+        std::cerr << "SSL connection failed\n";
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    // Build an HTTPS request
     std::string request = "GET " + path + " HTTP/1.1\r\nHost: " + std::string(hostName) + "\r\nConnection: close\r\n\r\n";
-    if (send(sock, request.c_str(), request.size(), 0) < 0) {
+    if (SSL_write(ssl, request.c_str(), request.size()) <= 0) {
         std::cerr << "Failed to send request\n";
         return 1;
     }
 
-    // Recv response
+    // Receive response
     char buffer[4096] = {};
     std::string response;
+    int bytesRecv;
 
-    for (;;) {
-        ssize_t bytesRecv = recv(sock, buffer, sizeof(buffer), 0);
-        if (bytesRecv <= 0) {
-            break;
+    do {
+        bytesRecv = SSL_read(ssl, buffer, sizeof(buffer));
+        if (bytesRecv > 0) {
+            response.append(buffer, bytesRecv);
         }
-        response.append(buffer, bytesRecv);
-    }
+    } while (bytesRecv > 0);
 
     std::cout << response << std::endl;
 
+    SSL_free(ssl);
     close(sock);
+    SSL_CTX_free(ctx);
 
     return 0;
 }
